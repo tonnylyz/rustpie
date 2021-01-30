2df4395f1381ed48eca2aa837d6c574a98ce7717
tonnylyz <lyztonny@gmail.com>
2021-01-30 15:08:50 +0800

    Replace native ld/ar with lld/rust-ar

--- build.rs
+++ build.rs
@@ -5,23 +5,23 @@ fn main() {
   let target = env::var("TARGET").expect("TARGET was not set");
   let out_dir = env::var("OUT_DIR").unwrap();
   if target.contains("riscv64") {
-    Command::new("riscv64-unknown-elf-ld")
-      .args(&["-r", "-b", "binary", "-o"])
+    Command::new("ld.lld")
+      .args(&["-m", "elf64lriscv", "-r", "-b", "binary", "-o"])
       .arg(&format!("{}/user_image.riscv64.o", out_dir))
       .arg("user/riscv64.elf")
       .status().unwrap();
-    Command::new("riscv64-unknown-elf-ar")
+    Command::new("rust-ar")
       .arg("crus")
       .arg(&format!("{}/libuserspace.a", out_dir))
       .arg(&format!("{}/user_image.riscv64.o", out_dir))
       .status().unwrap();
   } else if target.contains("aarch64") {
-    Command::new("aarch64-elf-ld")
-      .args(&["-r", "-b", "binary", "-o"])
+    Command::new("ld.lld")
+      .args(&["-m", "aarch64elf", "-r", "-b", "binary", "-o"])
       .arg(&format!("{}/user_image.aarch64.o", out_dir))
       .arg("user/aarch64.elf")
       .status().unwrap();
-    Command::new("aarch64-elf-ar")
+    Command::new("rust-ar")
       .arg("crus")
       .arg(&format!("{}/libuserspace.a", out_dir))
       .arg(&format!("{}/user_image.aarch64.o", out_dir))
