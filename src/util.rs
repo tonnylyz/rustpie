9df7e247db38657c4e972188ad09e797b7a8907e
tonnylyz <lyztonny@gmail.com>
2021-12-05 15:29:26 +0800

    Fix barrier

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,23 @@
-use spin::barrier::Barrier;
+// use spin::barrier::Barrier;
 
+use alloc::sync::Arc;
+use core::sync::atomic;
+use core::sync::atomic::{AtomicUsize, Ordering};
+use spin::Mutex;
 use crate::board::BOARD_CORE_NUMBER;
 
-static CORES_BARRIER: spin::Barrier = Barrier::new(BOARD_CORE_NUMBER);
+static COUNT: AtomicUsize = AtomicUsize::new(0);
 
 pub fn barrier() {
-  CORES_BARRIER.wait();
+  unsafe {
+    let count = COUNT.fetch_add(1, Ordering::AcqRel);
+    let next_count = round_up(count + 1, BOARD_CORE_NUMBER);
+    loop {
+      if COUNT.load(Ordering::Acquire) >= next_count {
+        break;
+      }
+    }
+  }
 }
 
 #[inline(always)]
