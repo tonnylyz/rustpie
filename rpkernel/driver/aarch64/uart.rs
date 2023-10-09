133882b3463078b204b7cdb95b02e6dadbf2d8d6
tonnylyz <lyztonny@gmail.com>
2023-10-09 21:15:57 +0800

    typo: correct name of rx fifo empty bit

--- rpkernel/driver/aarch64/uart.rs
+++ rpkernel/driver/aarch64/uart.rs
@@ -7,7 +7,7 @@ const UART_BASE: usize = 0xFFFF_FF80_0000_0000 + 0x900_0000;
 
 static PL011_MMIO: Pl011Mmio = Pl011Mmio::new(UART_BASE);
 
-const UART_FR_RXFF: u32 = 1 << 4;
+const UART_FR_RXFE: u32 = 1 << 4;
 const UART_FR_TXFF: u32 = 1 << 5;
 
 pub fn putc(c: u8) {
@@ -25,7 +25,7 @@ pub fn putc(c: u8) {
 
 pub fn getc() -> Option<u8> {
   let pl011 = &PL011_MMIO;
-  if pl011.Flag.get() & UART_FR_RXFF == 0 {
+  if pl011.Flag.get() & UART_FR_RXFE == 0 {
     Some((pl011.Data.get() & 0xff) as u8)
   } else {
     None
